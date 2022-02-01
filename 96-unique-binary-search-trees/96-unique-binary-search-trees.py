036c1f64bd01316891e04f4d83f630fa88f1f9f0
Charles Arnal <71833961+CharlesArnal@users.noreply.github.com>
2022-02-01 15:16:28 +0100

    Time: 45 ms (31.98%), Space: 14 MB (94.21%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -2,5 +2,5 @@ class Solution:
     def numTrees(self, n: int) -> int:
         num = [1,1] # 0 node and 1 node
         for i in range(2,n+1):
-            num.append(sum( (num[j-1]*num[i-j] for j in range(1,i+1)) ))
+            num.append(sum( [num[j-1]*num[i-j] for j in range(1,i+1)] ))
         return num[n]
