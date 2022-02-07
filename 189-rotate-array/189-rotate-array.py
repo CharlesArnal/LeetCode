d79ce1a9921cb7dfefa4954157c89e5e6fc385e5
Charles Arnal <71833961+CharlesArnal@users.noreply.github.com>
2022-02-07 18:12:29 +0100

    Time: 685 ms (18.32%), Space: 43.5 MB (10.44%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        L = len(nums)
+        gcd = np.gcd(L,k)
+        x = int(L/gcd)
+        for bc in range(gcd):
+            temp = nums[bc]
+            for i in range(0,x-1):
+                nums[(bc-i*k)%L] = nums[(bc-(i+1)*k)%L]
+            nums[(bc-(x-1)*k)%L] = temp
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
