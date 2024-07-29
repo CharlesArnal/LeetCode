f8b71df97eb83fe473bd840ceb1c367e0358d9c4
Charles Arnal <71833961+CharlesArnal@users.noreply.github.com>
2024-07-29 22:37:01 +0200

    Time: 39 ms (89.81%) | Memory: 16.5 MB (82.95%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -2,23 +2,26 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         translator = {"I":1,"V":5,"X":10,"L":50, "C":100, "D":500, "M" :1000}
         n = len(s)
-        # if n==0:
-        #     return 0
-        # total = 0
-        # for i, char in enumerate(s):
-        #     if i==n-1:
-        #         total += translator[char]
-        #     else:
-        #         if translator[char] > translator[s[i+1]]:
-        #             total+=
-
-        # return total
         if n==0:
             return 0
-        elif n == 1:
-            return translator[s]
-        else:
-            if translator[s[0]] >= translator[s[1]]:
-                return translator[s[0]] + self.romanToInt(s[1:])
-            if  translator[s[0]] < translator[s[1]]:
-                return translator[s[1]]-translator[s[0]] + self.romanToInt(s[2:])
+        total = 0
+        for i, char in enumerate(s):
+            if i==n-1:
+                total += translator[char]
+            else:
+                if translator[char]>= translator[s[i+1]]:
+                    total+= translator[char]
+                else:
+                    total-= translator[char]
+                
+
+        return total
+        # if n==0:
+        #     return 0
+        # elif n == 1:
+        #     return translator[s]
+        # else:
+        #     if translator[s[0]] >= translator[s[1]]:
+        #         return translator[s[0]] + self.romanToInt(s[1:])
+        #     if  translator[s[0]] < translator[s[1]]:
+        #         return translator[s[1]]-translator[s[0]] + self.romanToInt(s[2:])
